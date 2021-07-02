944e1ea4db43e6051736afaa5822ef9a36f1a0cb
esiale <blazejp0@gmail.com>
2021-07-03 00:20:55 +0200

    Added menu. Major code clean-up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,62 @@
 import "./style.css";
 import Background from "./bg.jpg";
-import test from "./menu.js";
+import renderMenu from "./menu.js";
 
-document.getElementById("content").classList.add("test");
+function createLayout() {
+    const content = document.getElementById("content");
 
-test();
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+
+    const main = document.createElement("main");
+
+    const footer = document.createElement("footer");
+    footer.textContent = "The Odin Project"
+
+    const header = document.createElement("header");
+
+    const headerLogo = document.createElement("div");
+    headerLogo.classList.add("header-logo");
+    headerLogo.textContent = "esiale Bistro";
+
+    const headerHome = document.createElement("div");
+    headerHome.textContent = "Home";
+    headerHome.addEventListener("click", renderHome);
+
+    const headerMenu = document.createElement("div");
+    headerMenu.textContent = "Menu";
+    headerMenu.addEventListener("click", renderMenu);
+
+    const headerContact = document.createElement("div");
+    headerContact.textContent = "Contact";
+    headerContact.addEventListener("click", renderContact);
+
+    header.append(headerLogo, headerHome, headerMenu, headerContact);
+    wrapper.append(header, main, footer);
+    content.append(wrapper);
+}
+
+function renderHome() {
+    clearMain("home");
+    const main = document.querySelector("main");
+
+    main.textContent = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum nesciunt, quibusdam, aliquid quod, at labore explicabo tempore quos perferendis minus officia nostrum perspiciatis architecto nobis maxime soluta harum vitae molestias magni id incidunt. Debitis placeat tempora veniam impedit dicta aut earum labore minus, deserunt velit at consectetur, accusamus, voluptatibus nobis neque eum quia expedita fuga deleniti architecto vero a voluptate. Illum eveniet repudiandae, rem officia veritatis quaerat error illo. Quod, reprehenderit sit earum porro nam vel aliquid numquam accusamus voluptates. Provident mollitia placeat asperiores a deleniti totam accusantium ratione quae, quibusdam veritatis obcaecati similique! Ducimus dolor nisi excepturi fugiat et!";
+}
+
+function clearMain(currentTab) {
+    const main = document.querySelector("main");
+    main.className = currentTab;
+
+    while(main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+}
+
+createLayout();
+renderHome();
+
+function renderContact() {
+    alert("Render Menu");
+}
+
+export default clearMain
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,72 @@
-const test = function() {
-    alert("IS THIS WORKING?");
+import clearMain from ".";
+import img0 from "./img/0.jpg";
+import img1 from "./img/1.jpg";
+import img2 from "./img/2.jpg";
+import img3 from "./img/3.jpg";
+import img4 from "./img/4.jpg";
+import img5 from "./img/5.jpg";
+
+const dish0 = new Image();
+const dish1 = new Image();
+const dish2 = new Image();
+const dish3 = new Image();
+const dish4 = new Image();
+const dish5 = new Image();
+
+dish0.src = img0;
+dish1.src = img1;
+dish2.src = img2;
+dish3.src = img3;
+dish4.src = img4;
+dish5.src = img5;
+
+
+const dishes = [
+    {
+        "image": dish0,
+        "desc": "25$ - A cake. Enough said."
+    },
+    {
+        "image": dish1,
+        "desc": "299$ - Cottage cheese. Delicious."
+    },
+    {
+        "image": dish2,
+        "desc": "66$ - An egg."
+    },
+    {
+        "image": dish3,
+        "desc": "3$ - Some fries."
+    },
+    {
+        "image": dish4,
+        "desc": "10$ - Juice."
+    },
+    {
+        "image": dish5,
+        "desc": "2$ - An apple."
+    }
+
+]
+
+
+
+function renderMenu() {
+    clearMain("menu");
+
+    for (let i = 0; i < dishes.length; i++ ) {
+        renderDish(`${i}`, dishes[i].image, dishes[i].desc)
+    }
+
+};
+
+const renderDish = function(id, image, desc) {
+    const main = document.querySelector("main");
+    const newDish = document.createElement("div");
+    const dishDesc = document.createElement("p");
+    dishDesc.textContent = desc;
+    newDish.append(image, dishDesc);
+    main.append(newDish);
 }
 
-export default test;
+export default renderMenu;
